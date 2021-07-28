6f7990d2919dc014447357c6eefd9ad3d195bc92
dan <73714708+dan1el5@users.noreply.github.com>
2021-07-28 18:28:51 -0400

    Add files via upload

---
+++ watermelon.py
@@ -0,0 +1,6 @@
+watermelon = int(input("Enter the weight of the watermelon: "))
+
+if watermelon % 2 == 0:
+    print("Weight can be split evenly")
+else:
+    print("Weight cannot be split evenly")
