671524a62c9ec91c016fd4c7a74167513c04bc8d
dan <73714708+dan1el5@users.noreply.github.com>
2021-08-16 22:31:46 -0400

    Add files via upload

---
+++ nextRound.py
@@ -0,0 +1,13 @@
+import random
+
+n = int(input("Enter number of contestants: "))
+k = int(input("Enter minimum score to qualify for the next round: "))
+qualified = 0
+
+for i in range(n):
+    scores = random.randint(0,100)
+    print(scores)
+    if scores >= k:
+        qualified+=1
+
+print(qualified, "people have qualified for the next round")
