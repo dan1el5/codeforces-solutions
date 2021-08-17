e660a7abf3dc5b11345695c70fe57ce367625fe4
dan <73714708+dan1el5@users.noreply.github.com>
2021-08-16 21:15:01 -0400

    Update team.py

--- team.py
+++ team.py
@@ -3,7 +3,6 @@ import random
 numProblems = int(input("Enter number of problems: "))
 total = 0
 
-
 for i in range(numProblems):
     petya, vasya, tonya = random.randint(0,1), random.randint(0,1), random.randint(0,1)
     print(petya, vasya, tonya)
