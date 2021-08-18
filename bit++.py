862fbafd3b0c5451e50eca5b0008ea8dcea3b528
dan <73714708+dan1el5@users.noreply.github.com>
2021-08-18 00:07:36 -0400

    Add files via upload
    uses a for loop instead of a while loop

--- bit++.py
+++ bit++.py
@@ -1,14 +1,11 @@
 numOfStatements = int(input("Enter the number of statements:"))
-
-i = 0
 count = 0
-while i < numOfStatements:
+
+for i in range(0, numOfStatements, 1):
     statement = input("Enter statement: ")
     if statement == "X++":
         count+=1
-        i+=1
     elif statement == "X--":
         count-=1
-        i+=1
 
 print(count)
